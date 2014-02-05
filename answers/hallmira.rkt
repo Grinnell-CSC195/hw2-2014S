ebc5ed86dfdcd78d0fa18868d693763d243347e7
Mira <m13hall@gmail.com>
2014-02-05 17:17:14 -0600

    added Mira's hw

---
+++ answers/hallmira.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require gigls/unsafe)
+(require psaifr/main)
+;; worked with Noah Schlager
+;2
+(image-show 
+ (psaifr-greyscale-illustration '(sign (+ x y)) 100 10))
+
+;3
+(image-show (psaifr-greyscale-illustration 
+             '(sign (- y x)) 100 10))
+
+;4
+(image-show (psaifr-greyscale-illustration 
+             '( - (sign ( - .75 (add (* x x) (* y y))))) 100 10))
+  
+;5
+(image-show (psaifr-greyscale-illustration 
+             '(sign (+ .75 y)) 100 10))
+
+;6
+(image-show (psaifr-greyscale-illustration 
+             '(sign (+ .6 x)) 100 10))
+
+;7
+(image-show (psaifr-greyscale-illustration 
+             '(- (sign (+ x y)) (sign (+ (+ .1 x) (+ .1 y)))) 100 10))
+
+
+;Problem Answer
+(image-show (psaifr-greyscale-illustration 
+             '(abs 
+               (- (* -1.5 (abs ( - .75 (add (* x x) (* y y))))) 
+                  (abs (- .5 (add (* x x) (* y y)))))) 100 10)) 
