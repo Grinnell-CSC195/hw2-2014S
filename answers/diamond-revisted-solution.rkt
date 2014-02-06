999915124422fab1da53cbf0613ed7a91e5454d3
Linda Oyolu <oyolulin17@cs.grinnell.edu>
2014-02-06 02:34:08 -0600

    Added my homework to the repo

---
+++ answers/diamond-revisted-solution.rkt
@@ -0,0 +1,8 @@
+#lang racket
+(require gigls/unsafe)
+(require psaifr/main)
+
+;Answer to Diamond Problem
+;(image-show (psaifr-greyscale-illustration 
+;             '(abs (/ (/ 5 x) (* 8 (tan y)))) 100 10))
+
